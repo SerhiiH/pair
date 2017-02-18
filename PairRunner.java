b6af281904d1414984a29f91311725ec21a9c594
SerhiiH <begofnl@gmail.com>
2017-02-18 23:37:29 +0200

    Add files via upload

---
+++ PairRunner.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Сергей on 18.02.2017.
+ */
+public class PairRunner {
+    public static void main(String[] args) {
+        Pair<Integer, Integer> pair = Pair.getPair(10, 20);
+        System.out.println(pair);
+    }
+}
